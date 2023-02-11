b5ed8c1c67bb761c6e436cd8c817c3c3605bb7a7
nenoNaninu <neno@melonpan.pro>
2023-02-11 16:53:49 +0900

    move contract types

--- src/TypedSignalR.Client.DevTools.UI/TranspilationSourceAttribute.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace Tapper;
-
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum)]
-internal class TranspilationSourceAttribute : Attribute
-{
-}
